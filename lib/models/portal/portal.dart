6174b4689e94375fd4e3af767cc45ec20a19c961
GreenArmy <GreenArmyOne@gmail.com>
2024-10-13 17:11:15 +1000

    remove factory constructors and internal maps from Portal interface

--- lib/models/portal/portal.dart
+++ lib/models/portal/portal.dart
@@ -6,32 +6,4 @@ abstract interface class Portal {
   String get code;
   SvgAssetLoader get logo;
   PortalService get service;
-
-  factory Portal.fromJson(Map<String, dynamic> json) =>
-      Portal.fromCode(json['code']);
-  Map<String, dynamic> toJson() => {'code': code};
-
-  factory Portal.fromCode(String code) {
-    try {
-      return portalsCodeMap[code]!;
-    } catch (e) {
-      throw ArgumentError('Invalid portal code');
-    }
-  }
-
-  factory Portal.fromUrl(Uri uri) {
-    try {
-      return _portalsUrlMap[uri.origin]!;
-    } catch (e) {
-      throw ArgumentError('Invalid portal url');
-    }
-  }
-
-  static void registerPortal(Portal portal) {
-    _portalsUrlMap[portal.url] = portal;
-    portalsCodeMap[portal.code] = portal;
-  }
 }
-
-final Map<String, Portal> _portalsUrlMap = {};
-final Map<String, Portal> portalsCodeMap = {};
